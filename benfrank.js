fe7de6ee3d7026bfae539a40f39163b6966514a0
Sagun Maharjan <sagun_90@hotmail.com>
2017-06-22 17:41:36 +0000

    event emitter

---
+++ benfrank.js
@@ -0,0 +1,12 @@
+// Handling events with eventEmitter
+//BenFrank.js
+
+var events = require('events');
+var emitter = new events.EventEmitter();
+
+emitter.on('customEvent', function(message,status){
+    console.log (`${status}:${message}`);
+});
+
+emitter.emit ('customEvent',"Hello World",200);
+
